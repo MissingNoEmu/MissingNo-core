e7d33c20d3b0c0d03d3c188372cf049fe821daf9
Divyansh Deep <manmica2018@gmail.com>
2026-09-21 15:43:53 +0530

    Commit farm

--- src/emulator.c
+++ src/emulator.c
@@ -2,7 +2,7 @@
 #include "internal/memory.h"
 #include "internal/opcodes.h"
 
-void execute(MN_Emu *emu, mn_u8 opcode)
+void mn_emulator_execute(MN_Emu *emu, mn_u8 opcode)
 {
   switch (opcode)
   {
